4b81a5f03c7776b296cfee643e00fc73968e0d48
Tran Thi Ly <lyt592004@gmail.com>
2024-08-16 14:16:59 +0700

    Add home and img

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Part 1</title>
+</head>
+<body>
+    <?php
+        echo "Hello, this is PHP script";
+        echo '<h1 style="color: orange; text-align: center;">Xin chao</h1>';
+        echo '<img style="width: 500px; height: 500px;" src="smile.png" alt="">';
+    ?>
+
+</body>
+</html>
